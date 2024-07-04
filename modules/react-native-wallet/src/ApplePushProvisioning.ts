7f5023b6054f1c9b8776b5e816520a33d58cd7f5
brunovjk <brunovjk@gmail.com>
2024-07-04 08:48:26 -0300

    Revert "Update ApplePushProvisioning.ts"
    This reverts commit 6982254012c15b422bdf56908eb143e59d24ed52.

--- modules/react-native-wallet/src/ApplePushProvisioning.ts
+++ modules/react-native-wallet/src/ApplePushProvisioning.ts
@@ -1,72 +1,48 @@
-import { NativeEventEmitter, NativeModules } from "react-native";
+import {NativeEventEmitter, NativeModules} from 'react-native';
 
-const { RNWallet } = NativeModules;
+const {RNWallet} = NativeModules;
 
 interface AddPassRequest {
-  last4: string;
-  cardHolder: string;
+    last4: string;
+    cardHolder: string;
 }
 
 interface CompletePassRequest {
-  activation: string;
-  encryptedData: string;
-  ephemeralKey: string;
+    activation: string;
+    encryptedData: string;
+    ephemeralKey: string;
 }
-type SupportedEvents =
-  | "addPaymentPassViewControllerDidFinish"
-  | "getPassAndActivation";
+
 const eventEmitter = new NativeEventEmitter(RNWallet);
 
 const ApplePushProvisioningModule = {
-  async canAddPass(): Promise<boolean> {
-    try {
-      return await RNWallet.canAddPaymentPass();
-    } catch {
-      throw new Error(`Error checking add pass capability.`);
-    }
-  },
-  async startAddPass(request: AddPassRequest): Promise<void> {
-    try {
-      return await RNWallet.startAddPaymentPass(
-        request.last4,
-        request.cardHolder
-      );
-    } catch (e) {
-      throw e;
-    }
-  },
-  async completeAddPass(request: CompletePassRequest): Promise<void> {
-    try {
-      return await RNWallet.completeAddPaymentPass(
-        request.activation,
-        request.encryptedData,
-        request.ephemeralKey
-      );
-    } catch (e) {
-      throw e;
-    }
-  },
-  addEventListener<T extends SupportedEvents>(
-    event: T,
-    callback: (
-      e: T extends "getPassAndActivation"
-        ? {
-            data: {
-              certificateLeaf: string;
-              certificateSubCA: string;
-              nonce: string;
-              nonceSignature: string;
-            };
-          }
-        : never
-    ) => void
-  ) {
-    eventEmitter.addListener(event, callback);
-  },
-
-  removeAllListeners(event: SupportedEvents) {
-    eventEmitter.removeAllListeners(event);
-  },
+    async canAddPass(): Promise<boolean> {
+        try {
+            return await RNWallet.canAddPass();
+        } catch {
+            throw new Error(`Error checking add pass capability.`);
+        }
+    },
+    async startAddPass(request: AddPassRequest): Promise<void> {
+        try {
+            return await RNWallet.startAddPass(request.last4, request.cardHolder);
+        } catch {
+            throw new Error(`Error starting add pass.`);
+        }
+    },
+    async completeAddPass(request: CompletePassRequest): Promise<void> {
+        try {
+            RNWallet.completeAddPass(request.activation, request.encryptedData, request.ephemeralKey);
+        } catch {
+            throw new Error(`Error completing add pass.`);
+        }
+    },
+    addEventListener(event: string, callback: (data: any) => void) {
+        eventEmitter.addListener(event, callback);
+    },
+    removeAllListeners(event: string) {
+        eventEmitter.removeAllListeners(event);
+    },
 };
 
 export default ApplePushProvisioningModule;
